64471876b3901bb4ee4059654c387edfd0a2c356
pemangodup <pngodup123@gmail.com>
2021-07-06 21:36:19 +0545

    first api url

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
-
-//load env
-dotenv.config({path: './config/config.env'});
+//load env vars
+dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+app.get('/api/v1/bootcamp', (req, res) => {
+  //res.send({ name: 'Pema Ngodup' });
+  // res.sendStatus(400).json({
+  //   success: false,
+  // });
+  // res.status(400).json({
+  //   success: false,
+  // });
+
+  res.status(200).json({ success: true, ms: 'Show all bootcamps' });
+});
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
