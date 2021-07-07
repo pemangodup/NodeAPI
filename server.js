b4e957a2ffb97d3290e7a15e76bbabf608f11eaf
pemangodup <pngodup123@gmail.com>
2021-07-07 16:03:52 +0545

    [controller and routes folder created]

--- server.js
+++ server.js
@@ -4,19 +4,13 @@ const dotenv = require('dotenv');
 //load env vars
 dotenv.config({ path: './config/config.env' });
 
+// Routes files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
 
-app.get('/api/v1/bootcamp', (req, res) => {
-  //res.send({ name: 'Pema Ngodup' });
-  // res.sendStatus(400).json({
-  //   success: false,
-  // });
-  // res.status(400).json({
-  //   success: false,
-  // });
-
-  res.status(200).json({ success: true, ms: 'Show all bootcamps' });
-});
+// Mount routers
+app.use('/app/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
